7e5707e5153520cfbc12cbac1c7ee935ed12363b
李佳宝 <12204926+Fern_weh@user.noreply.gitee.com>
2024-07-14 15:20:52 +0800

    前端显示报错2

--- scms-vue/src/components/admin/Schedule.vue
+++ scms-vue/src/components/admin/Schedule.vue
@@ -327,6 +327,10 @@ export default {
       axios
           .post("/racefootball/showRaceTable",_this.football)
           .then((res) => {
+            if (res.data.status != 200) {
+              return _this.$message.error(res.data.msg);
+            }
+            _this.$message.success("操作成功");
             let data = res.data.data;
             console.log(data)
             _this.treeData.data=data;
@@ -360,6 +364,7 @@ export default {
             if (res.data.status != 200) {
               return _this.$message.error(res.data.msg);
             }
+            _this.$message.success("操作成功");
             let data = res.data.data;
             _this.treeData.data=data;
             _this.page();
